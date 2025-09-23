1f17c77990e56872b0b6d30dbd7e59287b928990
Raed.Ghodhbane <ghodhbane.raed@gmail.com>
2025-09-23 15:30:50 +0200

    Enable "FeignClients" for "Account-service"

--- account-service/src/main/java/com/app/accountservice/AccountServiceApplication.java
+++ account-service/src/main/java/com/app/accountservice/AccountServiceApplication.java
@@ -2,8 +2,10 @@ package com.app.accountservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients(basePackages = "com.app.accountservice.service")
 public class AccountServiceApplication {
 
 	public static void main(String[] args) {
