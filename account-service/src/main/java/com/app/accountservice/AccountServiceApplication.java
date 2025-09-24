e794c4bcb8f5d016c34e6505aa7ae8cd52ae0596
Raed.Ghodhbane <ghodhbane.raed@gmail.com>
2025-09-24 13:02:01 +0200

    Add "DiscoveryClient" to "Account-service" microservice

--- account-service/src/main/java/com/app/accountservice/AccountServiceApplication.java
+++ account-service/src/main/java/com/app/accountservice/AccountServiceApplication.java
@@ -2,10 +2,12 @@ package com.app.accountservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients(basePackages = "com.app.accountservice.service")
+@EnableDiscoveryClient
+@EnableFeignClients(basePackages = "com.app.accountservice.clients")
 public class AccountServiceApplication {
 
 	public static void main(String[] args) {
