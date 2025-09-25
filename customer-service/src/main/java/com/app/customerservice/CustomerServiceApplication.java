48a0da2365534c3d850f6908ecf1de221346871a
Raed.Ghodhbane <ghodhbane.raed@gmail.com>
2025-09-25 12:49:57 +0200

    Add ""@EnableConfigurationProperties" to "customer-service" microservice

--- customer-service/src/main/java/com/app/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/app/customerservice/CustomerServiceApplication.java
@@ -1,9 +1,12 @@
 package com.app.customerservice;
 
+import com.app.customerservice.config.GlobalConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(GlobalConfig.class)
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
